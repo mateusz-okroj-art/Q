4353346145b7734a4d6f4dbc4bfc8c6247b338da
Mateusz Okrój <mateuszokroj1@gmail.com>
2023-03-08 00:33:32 +0100

    Configured basic projects.

---
+++ Core/ProjectIO/ProjectIO_Serializer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "QCoreProjectIOExport.h"
+
+namespace Q
+{
+	namespace Core
+	{
+		namespace ProjectIO
+		{
+			class Q_CORE_PROJECTIO_EXPORT Serializer
+			{
+
+			};
+		}
+	}
+}
---
+++ Core/ProjectIO/serializer.cpp
@@ -0,0 +1,1 @@
+#include "ProjectIO_Serializer.h"
