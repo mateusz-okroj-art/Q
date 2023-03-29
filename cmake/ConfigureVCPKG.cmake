7172f32849f82b2b281715b35848f4cb72c1c0b5
Mateusz Okrój <mateuszokroj1@gmail.com>
2023-03-29 15:36:11 +0200

    Modified linux apt configurator.

--- cmake/ConfigureVCPKG.cmake
+++ cmake/ConfigureVCPKG.cmake
@@ -8,12 +8,44 @@ function(ReadVCPKGConfigurationFile variable_name)
 	set(${variable_name} ${file_str} PARENT_SCOPE)
 endfunction()
 
-function(ConfigurePlatform platform_name)	
-	if(${platform_name} equal "Windows")
-		
-	elseif(${platform_name} equal "Linux")
+function(ConfigurePlatform json_file platform_name)	
+	if(${platform_name} STREQUAL "Windows")
+		# Currently no required
+	elseif(${platform_name} STREQUAL "Linux")
+		message(STATUS "Updating APT...")
 
-	elseif(${platform_name} equal "Darwin")
+		execute_process(
+			COMMAND apt update
+		)
+
+		message(STATUS "Upgrading APT packages...")
+
+		execute_process(
+			COMMAND apt upgrade
+		)
+
+		execute_process(
+			COMMAND apt list --installed
+			OUTPUT_VARIABLE out_var
+		)
+
+		#message(${out_var})
+
+		string(JSON arr_len LENGTH ${json_file})
+
+		foreach(i RANGE 0 ${arr_len})
+			string(JSON platform_description GET ${json_file} ${i})
+			string(JSON name GET ${platform_description} platform)
+
+			if(${name} STREQUAL ${platform_name})
+				string(JSON apt_required_packages GET ${json_file} apt-packages)
+
+				foreach(package_name ${apt_required_packages})
+
+				endforeach()
+			endif()
+		endforeach()
+	elseif(${platform_name} STREQUAL "Darwin")
 
 	else()
 		message(FATAL_ERROR "Configure VCPKG: Unsupported platform.")
@@ -28,6 +60,39 @@ function(GetVCPKGPath variable_name)
 	endif()
 
 	set(${variable_name} ${VCPKG} PARENT_SCOPE)
+endfunction()
+
+function(DetectPlatformAndTripletName platform_var triplet_var)
+	if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+		if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "AMD64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x64")
+			set(triplet "x64-windows")
+		elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "i686")
+			set(triplet "x86-windows")
+		elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+			set(triplet "arm64-windows")
+		else()
+			message(FATAL_ERROR "Unsupported architecture '${CMAKE_HOST_SYSTEM_PROCESSOR}'.")
+		endif()
+	elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+		if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "AMD64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x64")
+			set(triplet "x64-linux")
+		elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "i686")
+			set(triplet "x86-linux")
+		elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+			set(triplet "arm64-linux")
+		else()
+			message(FATAL_ERROR "Unsupported architecture '${CMAKE_HOST_SYSTEM_PROCESSOR}'.")
+		endif()
+	elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+		set(triplet "x64-osx")
+
+		#TODO implement support for Apple M1, M2 - arm64-osx
+	else()
+		message(FATAL_ERROR "Unsupported OS.")
+	endif()
+
+	set(${platform_var} ${CMAKE_SYSTEM_NAME} PARENT_SCOPE)
+	set(${triplet_var} ${triplet} PARENT_SCOPE)
 endfunction()
 
 function(ConfigureVCPKG)
@@ -46,6 +111,17 @@ function(ConfigureVCPKG)
 	)
 
 	execute_process(COMMAND ${vcpkg_app} update)
+
+	message(STATUS "Reading packages.json...")
+	ReadVCPKGConfigurationFile(packages_json)
+
+	DetectPlatformAndTripletName(platform_name triplet)
+	message("Detected platform: ${platform_name} - ${triplet}")
+
+	message(STATUS "Configuring platform...")
+	ConfigurePlatform(${packages_json} ${platform_name})
+
+
 endfunction()
 
 function(Build_VCPKG vcpkg_libraries)
@@ -107,32 +183,6 @@ function(Build_VCPKG vcpkg_libraries)
 		WORKING_DIRECTORY ${vcpkg_dir}
 		COMMAND ./vcpkg update
 	)
-
-	if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-		if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "AMD64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x64")
-			set(triplet "x64-windows")
-		elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "i686")
-			set(triplet "x86-windows")
-		elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-			set(triplet "arm64-windows")
-		else()
-			message(FATAL_ERROR "Unsupported architecture '${CMAKE_HOST_SYSTEM_PROCESSOR}'.")
-		endif()
-	elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-		if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "AMD64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x64")
-			set(triplet "x64-linux")
-		elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "i686")
-			set(triplet "x86-linux")
-		elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-			set(triplet "arm64-linux")
-		else()
-			message(FATAL_ERROR "Unsupported architecture '${CMAKE_HOST_SYSTEM_PROCESSOR}'.")
-		endif()
-	elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
-		set(triplet "x64-osx")
-	else()
-		message(FATAL_ERROR "Unsupported OS.")
-	endif()
 
 	foreach(package_name ${${vcpkg_libraries}})
 		message(STATUS "vcpkg: Installing '${package_name}'...")
